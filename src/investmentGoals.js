ac35e2d753ac431aa44d955e269f82a93fbf26f1
Marcelo S Carvalho <marceloscarvalho100@gmail.com>
2024-10-22 18:26:00 -0300

    formulário completo

---
+++ src/investmentGoals.js
@@ -0,0 +1,44 @@
+function convertToMonthlyReturnRate(yearRerturnRate) {
+  return yearRerturnRate ** (1 / 12);
+}
+export function generateReturnsArray(
+  startingAmount = 0,
+  timeHorize = 0,
+  timePeriod = "monthly",
+  monthlyContribution = 0,
+  returnRate = 0,
+  returnTimeFrame = "monthly"
+) {
+  if (!timeHorize || !startingAmount) {
+    throw new Error("Please provide a time horize and starting amount.");
+  }
+
+  const finalReturnRate = returnTimeFrame === "monthly" ? 1 + returnRate / 100 : convertToMonthlyReturnRate(1 + returnRate / 100);
+  const finalTimeHorize = timePeriod === "monthly" ? timeHorize : timeHorize * 12;
+  const referenceInvestmentObject = {
+    investedAmount: startingAmount,
+    interestReturns: 0,
+    totalInterestReturns: 0,
+    month: 0,
+    totalAmount: startingAmount,
+  };
+
+  const returnsArray = [referenceInvestmentObject];
+
+  for (let timeReference = 1; timeReference <= finalTimeHorize; timeReference++) {
+    const totalAmount = returnsArray[timeReference - 1].totalAmount * finalReturnRate + monthlyContribution;
+
+    const interestReturns = returnsArray[timeReference - 1].totalAmount * finalReturnRate;
+    const investedAmount = startingAmount + monthlyContribution * timeReference;
+    const totalInterestReturns = totalAmount + investedAmount * timeReference;
+
+    returnsArray.push({
+      investedAmount: investedAmount,
+      interestReturns: interestReturns,
+      totalInterestReturns: totalInterestReturns,
+      month: timeReference,
+      totalAmount: totalAmount,
+    });
+  }
+  return returnsArray;
+}
